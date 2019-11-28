f52a1120203eba69615bde766c559e7753a077dd
Daria Farina <daria.farina@extrawest.com>
2019-11-28 10:34:14 +0200

    created 2 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
