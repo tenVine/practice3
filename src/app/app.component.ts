e7a26cd226b38cd5b0b9cada65ba872157670e9a
Daria Farina <daria.farina@extrawest.com>
2019-11-28 18:28:50 +0200

    message and color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   heading: string = 'Practice3.1';
+  timer: any;
+  timeLeft: number = 10;
+  changeColor: boolean = true;
+  changeText: string = 'Timer Works!!'
+
+  ngOnInit() {
+    
+    
+    // this.check();
+    this.start()
+  }
+  // check() {
+  //   if(this.timeLeft = 0) {
+  //     alert(1)
+  //       this.changeColor = !this.changeColor;
+  //     }
+  // }
+  start() {
+    this.timer = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.pause();
+        this.changeColor = !this.changeColor;
+        this.changeText = 'Timer Stoped';
+      }
+    },1000)
+  }
+  
+  pause() {
+      clearInterval(this.timer);
+    
+  }
+
+  
 }
